8d1c73f9d6ca4223bb2c1dad52b97981914df219
Kazuki Maeda <kmaeda@kmaeda.net>
2016-05-01 03:21:48 +0900

    Add create_archive.sh.

---
+++ create_archive.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PROJECT=platex-base
+TMP=/tmp
+
+git archive --format=tar --prefix=$PROJECT/ HEAD | (cd $TMP && tar xf -)
+
+# Remove generated and auxiliary files
+rm $TMP/$PROJECT/.gitignore
+rm $TMP/$PROJECT/create_archive.sh
+rm $TMP/$PROJECT/kinsoku.tex
+rm $TMP/$PROJECT/pl209.def
+ls $TMP/$PROJECT/*.cls | grep -v plnews.cls | grep -v jltxdoc.cls | xargs rm
+rm $TMP/$PROJECT/*.clo
+rm $TMP/$PROJECT/*.fd
+ls $TMP/$PROJECT/*.ltx | grep -v plpatch.ltx | xargs rm
+rm $TMP/$PROJECT/*.sty
+
+echo " * Create $PROJECT.zip"
+
+PWDF=`pwd`
+cd $TMP && zip -r $PWDF/$PROJECT.zip $PROJECT
+rm -rf $TMP/$PROJECT
+echo
+echo " * Done: $PROJECT.zip"
